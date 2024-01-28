1a7a961cf245565c140ea94d793e38d5a518404b
xenobiont <61256244+xenobiont@users.noreply.github.com>
2024-01-28 05:48:43 +0200

    Initial commit

---
+++ task/task.js
@@ -0,0 +1,10 @@
+const breakfeast = ['bacon', 'eggs', 'tomatoes', 'mushrooms', 'tea', 'coffee'];
+
+let dinner;
+
+// your code here
+
+dinner = breakfeast.splice(4);
+console.log('dinner', dinner);
+
+console.log('breakfast', breakfeast);
---
+++ task/task.md
@@ -0,0 +1,3 @@
+# Task
+
+If 'breakfast' array has more then 4 items, transfer the rest for dinner (in dinner array). Print both arrays to console
